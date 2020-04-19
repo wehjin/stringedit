d034d95726514a69d233c912422f3df48842d7aa
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2020-04-19 05:59:07 -0700

    Adds StringEdit::cursor_left().

--- src/navigate.rs
+++ src/navigate.rs
@@ -17,11 +17,32 @@ mod tests {
 			assert_eq!(edit, StringEdit { chars: vec!['a'], cursor_pos: 1 })
 		}
 	}
+
+	mod cursor_left {
+		use crate::StringEdit;
+
+		#[test]
+		fn from_far_left() {
+			let edit = StringEdit::new("a", 0).cursor_left();
+			assert_eq!(edit, StringEdit { chars: vec!['a'], cursor_pos: 0 })
+		}
+
+		#[test]
+		fn from_far_right() {
+			let edit = StringEdit::new("a", 1).cursor_left();
+			assert_eq!(edit, StringEdit { chars: vec!['a'], cursor_pos: 0 })
+		}
+	}
 }
 
 impl StringEdit {
 	pub fn cursor_right(&self) -> Self {
 		let cursor_pos = (self.cursor_pos + 1).min(self.chars.len());
+		StringEdit { chars: self.chars.to_vec(), cursor_pos }
+	}
+
+	pub fn cursor_left(&self) -> Self {
+		let cursor_pos = if self.cursor_pos > 0 { self.cursor_pos - 1 } else { self.cursor_pos };
 		StringEdit { chars: self.chars.to_vec(), cursor_pos }
 	}
 }
